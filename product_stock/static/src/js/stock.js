ee3c6f01564d9132ba9951d2add1b42a80b89c02
susmitha <susmitharajkc7@gmail.com>
2020-11-25 23:02:19 +0530

    [ADD]product_stock

---
+++ product_stock/static/src/js/stock.js
@@ -0,0 +1,28 @@
+odoo.define('point_of_sale.StockProduct', function (require) {
+"use strict";
+//    const { Gui } = require('point_of_sale.Gui');
+//    const ProductItem = require('point_of_sale.ProductItem');
+//    const Registries = require('point_of_sale.Registries');
+
+    var models = require('point_of_sale.models');
+//    models.load_fields('stock.quant', ['inventory_quantity']);
+//    console.log("this props",this.props);
+//    models.load_fields('product.product', ['qty_available']);
+
+    console.log(models);
+    console.log("xxxxxxxxxxxx");
+    models.load_fields('product.product', ['type', 'qty_available', 'virtual_available']);
+
+//    models.load_models({
+//        model: 'product.product',
+//        fields: ['qty_avaliable',],
+
+//        loaded: function (self, invoices) {
+//          var invoices_ids = _.pluck(invoices, 'id');
+//          self.prepare_invoices_data(invoices);
+//          self.invoices = invoices;
+//          self.db.add_invoices(invoices);
+//          self.get_invoice_lines(invoices_ids);
+//      }
+});
+
